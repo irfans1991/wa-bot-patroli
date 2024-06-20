71635aff35a81b0e47c30dcd758fdea50ccb67b6
irfans1991 <irfan.developer19@gmail.com>
2024-06-20 11:19:44 +0800

    memperbaiki send message wa bot

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 	"whatsapp-bot/database"
 	"whatsapp-bot/domain"
+	"whatsapp-bot/handler"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/mdp/qrterminal/v3"
@@ -106,6 +108,7 @@ func PollNewActivities(db *gorm.DB) {
 	ctx = context.Background()
 	var lastCheckedID uint = 0
 	var message string
+	var Unit string
 
 	for {
 		var mutasi_masuks []domain.Mutasi_masuks
@@ -118,27 +121,54 @@ func PollNewActivities(db *gorm.DB) {
 			fmt.Printf("New activity detected: %v\n", activity)
 			lastCheckedID = uint(activity.Id)
 			dates := activity.Created_at.Format("2006-01-02")
-			message = activity.Unit
+			Unit = activity.Unit
 
 			switch {
-			case message == "Boks" && dates == timeNow:
-				fmt.Println("ada barang boks masuk")
-			case message == "Colly" && dates == timeNow:
-				fmt.Println("ada barang colly masuk")
-			case message == "Galon" && dates == timeNow:
-				fmt.Println("ada barang galon masuk")
-			case message == "Botol" && dates == timeNow:
-				fmt.Println("ada barang botol masuk")
-			case message == "Roll" && dates == timeNow:
-				fmt.Println("ada barang roll masuk")
-			case message == "Tabung" && dates == timeNow:
-				fmt.Println("ada barang tabung masuk")
-			case message == "Pcs" && dates == timeNow:
-				fmt.Println("ada barang pcs masuk")
-			case message == "Pail" && dates == timeNow:
-				fmt.Println("ada barang pail masuk")
-			case message == "Liter" && dates == timeNow:
-				fmt.Println("ada barang liter masuk")
+			case Unit == "Boks" && dates == timeNow:
+				message = fmt.Sprintf("Halo kak, ada barang %s Colly masuk di area perusahaan dari %s, diterima oleh Pak %s, silahkan hubungi security, Terimakasih !", activity.TotalItems, activity.Supplier, activity.SecurityName)
+				if err := handler.SendMessages(ctx, client, message); err != nil {
+					log.Fatalf("Failed to send Unit: %v", err)
+				}
+			case Unit == "Colly" && dates == timeNow:
+				message = fmt.Sprintf("Halo kak, ada barang %s Colly masuk di area perusahaan dari %s, diterima oleh Pak %s, silahkan hubungi security, Terimakasih !", activity.TotalItems, activity.Supplier, activity.SecurityName)
+				if err := handler.SendMessages(ctx, client, message); err != nil {
+					log.Fatalf("Failed to send Unit: %v", err)
+				}
+			case Unit == "Galon" && dates == timeNow:
+				message = fmt.Sprintf("Halo kak, ada Air %s Galon masuk di area perusahaan dari %s, diterima oleh Pak %s, silahkan hubungi security, Terimakasih !", activity.TotalItems, activity.Supplier, activity.SecurityName)
+				if err := handler.SendMessages(ctx, client, message); err != nil {
+					log.Fatalf("Failed to send Unit: %v", err)
+				}
+			case Unit == "Botol" && dates == timeNow:
+				message = fmt.Sprintf("Halo kak, ada %s Botol masuk di area perusahaan dari %s, diterima oleh Pak %s, silahkan hubungi security, Terimakasih !", activity.TotalItems, activity.Supplier, activity.SecurityName)
+				if err := handler.SendMessages(ctx, client, message); err != nil {
+					log.Fatalf("Failed to send Unit: %v", err)
+				}
+			case Unit == "Roll" && dates == timeNow:
+				message = fmt.Sprintf("Halo kak, ada Barang %s Roll masuk di area perusahaan dari %s, diterima oleh Pak %s, silahkan hubungi security, Terimakasih !", activity.TotalItems, activity.Supplier, activity.SecurityName)
+				if err := handler.SendMessages(ctx, client, message); err != nil {
+					log.Fatalf("Failed to send Unit: %v", err)
+				}
+			case Unit == "Tabung" && dates == timeNow:
+				message = fmt.Sprintf("Halo kak, ada %s Tabung masuk di area perusahaan dari %s, diterima oleh Pak %s, silahkan hubungi security, Terimakasih !", activity.TotalItems, activity.Supplier, activity.SecurityName)
+				if err := handler.SendMessages(ctx, client, message); err != nil {
+					log.Fatalf("Failed to send Unit: %v", err)
+				}
+			case Unit == "Pcs" && dates == timeNow:
+				message = fmt.Sprintf("Halo kak, ada Barang %s Pcs masuk di area perusahaan dari %s, diterima oleh Pak %s, silahkan hubungi security, Terimakasih !", activity.TotalItems, activity.Supplier, activity.SecurityName)
+				if err := handler.SendMessages(ctx, client, message); err != nil {
+					log.Fatalf("Failed to send Unit: %v", err)
+				}
+			case Unit == "Pail" && dates == timeNow:
+				message = fmt.Sprintf("Halo kak, ada %s pail masuk di area perusahaan dari %s, diterima oleh Pak %s, silahkan hubungi security, Terimakasih !", activity.TotalItems, activity.Supplier, activity.SecurityName)
+				if err := handler.SendMessages(ctx, client, message); err != nil {
+					log.Fatalf("Failed to send Unit: %v", err)
+				}
+			case Unit == "Liter" && dates == timeNow:
+				message = fmt.Sprintf("Halo kak, ada %s Liter masuk di area perusahaan dari %s, diterima oleh Pak %s, silahkan hubungi security, Terimakasih !", activity.TotalItems, activity.Supplier, activity.SecurityName)
+				if err := handler.SendMessages(ctx, client, message); err != nil {
+					log.Fatalf("Failed to send Unit: %v", err)
+				}
 			}
 
 			// if message == "Jhins" && dates == timeNow {
