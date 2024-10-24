a5c0444d9eb05c14bbb696de1c13a823ee479a46
irfans1991 <irfan.developer19@gmail.com>
2024-10-24 11:28:08 +0800

    perbaiki activity message pool

--- usecase/pollNewActivities.go
+++ usecase/pollNewActivities.go
@@ -34,11 +34,11 @@ func PollNewActivities(db *gorm.DB, client *whatsmeow.Client) {
 			dates := activity.Created_at.Format("2006-01-02")
 			switch {
 			// manado activity
-			case activity.Supplier == "IKAN" && dates == timeNow:
-				message = fmt.Sprintf("Halo kak, ada Ikan masuk di area perusahaan dari supplier %s, dengan total items %s %s, diterima oleh Pak %s,\n *_dengan informasi berikut : %s_*, \n silahkan hubungi security, Terimakasih !", activity.Supplier_Name, activity.TotalItems, activity.Unit, activity.Security, activity.Remark)
-				if err := handler.SendMessages(ctx, client, message); err != nil {
-					log.Fatalf("Failed to send Unit: %v", err)
-				}
+			// case activity.Supplier == "IKAN" && dates == timeNow:
+			// 	message = fmt.Sprintf("Halo kak, ada Ikan masuk di area perusahaan dari supplier %s, dengan total items %s %s, diterima oleh Pak %s,\n *_dengan informasi berikut : %s_*, \n silahkan hubungi security, Terimakasih !", activity.Supplier_Name, activity.TotalItems, activity.Unit, activity.Security, activity.Remark)
+			// 	if err := handler.SendMessages(ctx, client, message); err != nil {
+			// 		log.Fatalf("Failed to send Unit: %v", err)
+			// 	}
 			case activity.Type_Mutasi == "Masuk Barang" && dates == timeNow:
 				message = fmt.Sprintf("Halo kak, ada barang %s %s masuk di area perusahaan dari %s, %s, diterima oleh Pak %s,\n *_dengan informasi berikut : %s_*, \n silahkan hubungi security, Terimakasih !", activity.TotalItems, activity.Unit, activity.Supplier_Name, activity.From, activity.Security, activity.Remark)
 				if err := handler.SendMessages(ctx, client, message); err != nil {
@@ -59,38 +59,36 @@ func PollNewActivities(db *gorm.DB, client *whatsmeow.Client) {
 
 }
 
-// for {
-// 	var mutasi_masuks []domain.Mutasi_masuks
+// func PollNewActivities(db *gorm.DB, client *whatsmeow.Client) {
+// 	ctx = context.Background()
+// 	var lastCheckedID uint = 0
+// 	var message string
+// 	var Unit string
 
-// 	db.Where("id > ?", lastCheckedID).Order("id asc").Find(&mutasi_masuks)
-// 	timeNow := time.Now().Format("2006-01-02")
+// 	for {
+// 		var mutasi_masuks []domain.Mutasi_masuks
 
-// 	for _, activity := range mutasi_masuks {
+// 		db.Where("id > ?", lastCheckedID).Order("id asc").Find(&mutasi_masuks)
+// 		timeNow := time.Now().Format("2006-01-02")
 
-// 		// fmt.Printf("New activity detected: %v\n", activity)
-// 		lastCheckedID = uint(activity.Id)
-// 		dates := activity.Created_at.Format("2006-01-02")
-// 		// Unit = activity.Unit
-// 		switch
-// 		// manado activity
-// 			case activity.Supplier == "IKAN" && dates == timeNow:
-// 				message = fmt.Sprintf("Halo kak, ada Ikan masuk di area perusahaan dari supplier %s, dengan total items %s, diterima oleh Pak %s,\n *_dengan informasi berikut : %s_*, \n silahkan hubungi security, Terimakasih !", activity.Supplier_Name, activity.Unit, activity.Security, activity.Remark)
+// 		for _, activity := range mutasi_masuks {
+
+// 			fmt.Printf("New activity detected: %v\n", activity)
+// 			lastCheckedID = uint(activity.Id)
+// 			dates := activity.Created_at.Format("2006-01-02")
+// 			Unit = activity.Unit
+
+// 			switch {
+// 			case Unit == "Boks" && dates == timeNow && activity.Type_Mutasi == "Masuk Barang":
+// 				message = fmt.Sprintf("Halo kak, ada barang %s Boks masuk di area perusahaan dari %s, diterima oleh Pak %s, silahkan hubungi security, Terimakasih !", activity.TotalItems, activity.Supplier_Name, activity.Security)
 // 				if err := handler.SendMessages(ctx, client, message); err != nil {
 // 					log.Fatalf("Failed to send Unit: %v", err)
 // 				}
-// 			case activity.Type_Mutasi == "Masuk Barang" && dates == timeNow:
-// 				message = fmt.Sprintf("Halo kak, ada barang %s %s masuk di area perusahaan dari %s, %s, diterima oleh Pak %s,\n *_dengan informasi berikut : %s_*, \n silahkan hubungi security, Terimakasih !", activity.TotalItems, activity.Unit, activity.Supplier_Name, activity.From, activity.Security, activity.Remark)
-// 				if err := handler.SendMessages(ctx, client, message); err != nil {
-// 					log.Fatalf("Failed to send Unit: %v", err)
-// 				}
-// 			case activity.Type_Mutasi == "Keluar" && dates == timeNow:
-// 				message = fmt.Sprintf("Halo kak, ada aktifitas barang Keluar, dengan informasi : *_%s_* , dari supplier %s ke %s, \n silahkan hubungi security untuk informasi lebih lanjut, Terimakasih !", activity.Remark, activity.Supplier_Name, activity.From)
-// 				if err := handler.SendMessages(ctx, client, message); err != nil {
-// 					log.Fatalf("Failed to send Unit: %v", err)
-// 				}
+// 			}
 // 		}
+
+// 		// Sleep for a while before the next poll
+// 		time.Sleep(10 * time.Second)
 // 	}
 
-// 	// Sleep for a while before the next poll
-// 	time.Sleep(10 * time.Second)
 // }
