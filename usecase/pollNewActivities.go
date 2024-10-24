43ee83da695a8abc5ca61e498a9369f0945cdef6
irfans1991 <irfan.developer19@gmail.com>
2024-10-24 14:02:00 +0800

    fixed pol mutasi masuks

--- usecase/pollNewActivities.go
+++ usecase/pollNewActivities.go
@@ -29,11 +29,20 @@ func PollNewActivities(db *gorm.DB, client *whatsmeow.Client) {
 		timeNow := time.Now().Format("2006-01-02")
 
 		for _, activity := range mutasi_masuks {
-			fmt.Printf("New activity detected: %v\n", activity)
 			lastCheckedID = uint(activity.Id)
 			dates := activity.Created_at.Format("2006-01-02")
 			if activity.Type_Mutasi == "Masuk" && activity.Supplier == "IKAN" && dates == timeNow {
 				message = fmt.Sprintf("Halo kak, ada Ikan masuk di area perusahaan dari supplier %s, dengan total items %s %s, diterima oleh Pak %s,\n *_dengan informasi berikut : %s_*, \n silahkan hubungi security, Terimakasih !", activity.Supplier_Name, activity.TotalItems, activity.Unit, activity.Security, activity.Remark)
+				if err := handler.SendMessages(ctx, client, message); err != nil {
+					log.Fatalf("Failed to send Unit: %v", err)
+				}
+			} else if activity.Type_Mutasi == "Masuk" && dates == timeNow {
+				message = fmt.Sprintf("Halo kak, ada barang %s %s masuk di area perusahaan dari %s, %s, diterima oleh Pak %s,\n *_dengan informasi berikut : %s_*, \n silahkan hubungi security, Terimakasih !", activity.TotalItems, activity.Unit, activity.Supplier_Name, activity.From, activity.Security, activity.Remark)
+				if err := handler.SendMessages(ctx, client, message); err != nil {
+					log.Fatalf("Failed to send Unit: %v", err)
+				}
+			} else if activity.Type_Mutasi == "Keluar" && dates == timeNow {
+				message = fmt.Sprintf("Halo kak, ada aktifitas barang Keluar, dengan informasi : *_%s_* , dari supplier %s ke %s, \n silahkan hubungi security untuk informasi lebih lanjut, Terimakasih !", activity.Remark, activity.Supplier_Name, activity.From)
 				if err := handler.SendMessages(ctx, client, message); err != nil {
 					log.Fatalf("Failed to send Unit: %v", err)
 				}
